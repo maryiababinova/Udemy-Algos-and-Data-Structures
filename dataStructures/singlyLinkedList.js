a51e07db44c4c57d121f915e24f163c194c4749e
Maryia Miller <maryiamiller@gmail.com>
2019-08-12 16:11:53 +0300

    dll push

--- dataStructures/singlyLinkedList.js
+++ dataStructures/singlyLinkedList.js
@@ -110,4 +110,19 @@ class SinglyLinkedList {
     this.length--;
     return removed;
   }
+
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let prev = null;
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
 }
