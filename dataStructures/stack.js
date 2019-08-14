b6920925f2b0706be459a78407c9882c3a9108f2
Maryia Miller <maryiamiller@gmail.com>
2019-08-14 17:37:36 +0300

    stack

---
+++ dataStructures/stack.js
@@ -0,0 +1,36 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  push(val) {
+    let node = new Node(val);
+    if (!this.length) {
+      this.first = node;
+      this.last = node;
+    } else {
+      let temp = this.first;
+      this.first = node;
+      this.first.next = temp;
+    }
+    return ++this.length;
+  }
+  pop() {
+    if (!this.length) return null;
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.length--;
+    return temp.val;
+  }
+}
