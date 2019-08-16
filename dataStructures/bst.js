a99de71c20181549f0af3f653b812602482fec55
Maryia Miller <maryiamiller@gmail.com>
2019-08-16 14:53:51 +0300

    insert

--- dataStructures/bst.js
+++ dataStructures/bst.js
@@ -10,4 +10,30 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(val) {
+    let node = new Node(val);
+    if (!this.root) {
+      this.root = node;
+      return this;
+    }
+    let current = this.root;
+
+    while (true) {
+      if (current.val === val) return undefined;
+      if (current.val > val) {
+        if (!current.left) {
+          current.left = node;
+          return this;
+        }
+        current = current.left;
+      } else if (current.val < val) {
+        if (!current.right) {
+          current.right = node;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
 }
