eef1205cf143c29123080eae188c6e91c3b1c5a1
Maryia Miller <maryiamiller@gmail.com>
2019-08-20 19:01:27 +0100

    rec

---
+++ recursion/recursion.js
@@ -0,0 +1,24 @@
+const power = (n, p) => {
+  if (p === 0) return 1;
+  return n * power(p - 1);
+};
+
+const factorial = n => {
+  if (n === 1 || n === 0) return 1;
+  return n * factorial(n - 1);
+};
+
+const productOfArray = arr => {
+  if (!arr.length) return 1;
+  return arr[0] * productOfArray(arr.slice(1));
+};
+
+const recursiveRange = n => {
+  if (n === 0) return 0;
+  return n + recursiveRange(n - 1);
+};
+
+const fib = n => {
+  if (n <= 2) return 1;
+  return fib(n - 2) + fib(n - 1);
+};
